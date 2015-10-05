8a2c0f4b14d7d74887dd60991f63e4809c77f06a
Christofer17 <christopher.acevedo117@gmail.com>
2015-10-05 16:58:39 -0600

    I corrected validations

--- application.py
+++ application.py
@@ -1,3 +1,99 @@
-# Aqui escribe tu codigo
+import os
+import sys
 
-print "Hello World"
+al = {}
+Country = []
+Capital = []
+
+
+def question():
+    var3=raw_input("Do you want to enter another country? y/n: " )
+    var3=var3.lower()
+    if var3 == "y":
+        Insert_Country()
+    elif var3== "n":
+        menu()
+    else:
+        print "This is not valid"
+        limpiar()
+        question()
+
+def Insert_Country():
+    limpiar()
+    mas = True
+    while mas == True:
+        var1 =raw_input("Enter the Country: ")
+        if var1.isalpha() or " " in var1:
+            Country.append(var1)
+            mas = False
+        else:
+            print "only words"
+            mas = True
+    while mas == False:
+        var2 =raw_input("Enter the Capital: ")
+        if var2.isalpha() or " " in var2:
+            Capital.append(var2)
+            mas = True
+        else:
+            print "only words"
+            mas = False
+    
+    al[var1]=var2
+    question()    
+    menu()
+
+
+def Country_List():
+    limpiar()
+    for i in Country:
+        print i 
+    raw_input("Press enter")
+    limpiar()
+    menu()
+
+def Capital_List():
+    for i in Capital:
+        print i 
+    raw_input("Press enter")
+    limpiar()
+    menu()
+
+def view_all():
+    for i in al:
+        print i, al[i]
+    raw_input("presione enter ")
+
+
+def limpiar():
+    os.system("reset")
+
+
+def salir():
+    sys.exit()
+    
+
+def menu():
+    limpiar()
+    print "\t\t\t****Countries and Capitals****\n"
+    print "*~~~What do you want to do?~~~*"
+    print "1. Insert your Country"
+    print "2. Countrie List"
+    print "3. Capital List"
+    print "4. view All"
+    print "5. Exit/Quit\n"
+    menu = raw_input("Insert your option: ")
+
+    if menu == "1":
+        Insert_Country()
+    elif menu == "2":
+        Country_List()
+    elif menu == "3":
+        Capital_List()
+    
+    elif menu == "5":
+        salir()
+    elif menu=="4":
+        view_all()
+    else:
+        print "You have not entered anything"
+menu()
