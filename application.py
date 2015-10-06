b245d8bc176c9a3af7aa9eabd131f052f1108660
Christofer17 <christopher.acevedo117@gmail.com>
2015-10-06 17:06:47 -0600

    Finished

--- application.py
+++ application.py
@@ -1,5 +1,11 @@
+from collections import OrderedDict
 import os
 import sys
+import time
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+import getpass
 
 al = {}
 Country = []
@@ -7,14 +13,14 @@ Capital = []
 
 
 def question():
-    var3=raw_input("Do you want to enter another country? y/n: " )
+    var3=raw_input("-You want to enter another country with capital? YES or NO: " )
     var3=var3.lower()
-    if var3 == "y":
+    if var3 == "y" or var3 == "yes" or var3 == "YES" or var3 == "Yes":
         Insert_Country()
-    elif var3== "n":
+    elif var3== "n" or var3 == "no" or var3 == "NO" or var3 == "No":
         menu()
     else:
-        print "This is not valid"
+        print "'-->This is not valid<--'"
         limpiar()
         question()
 
@@ -24,16 +30,18 @@ def Insert_Country():
     while mas == True:
         var1 =raw_input("Enter the Country: ")
         if var1.isalpha() or " " in var1:
-            Country.append(var1)
-            mas = False
+        	var1 = var1.capitalize()
+        	Country.append(var1)
+        	mas = False
         else:
             print "only words"
             mas = True
     while mas == False:
         var2 =raw_input("Enter the Capital: ")
         if var2.isalpha() or " " in var2:
-            Capital.append(var2)
-            mas = True
+        	var2 = var2.capitalize()
+        	Capital.append(var2)
+        	mas = True
         else:
             print "only words"
             mas = False
@@ -44,56 +52,132 @@ def Insert_Country():
 
 
 def Country_List():
-    limpiar()
-    for i in Country:
-        print i 
-    raw_input("Press enter")
-    limpiar()
-    menu()
+	limpiar()
+	print "*~~~COUNTRY LIST~~~*"
+	for i in Country:
+		print i 
+	raw_input("Press enter")
+	limpiar()
+	menu()
 
 def Capital_List():
-    for i in Capital:
-        print i 
-    raw_input("Press enter")
-    limpiar()
-    menu()
+	limpiar()
+	print "*~~~CAPITAL LIST~~~*"
+	for i in Capital:
+		print i 
+	raw_input("Press enter")
+	limpiar()
+	menu()
 
 def view_all():
-    for i in al:
-        print i, al[i]
-    raw_input("presione enter ")
+	limpiar()
+	print "*~~~Here print you countries~~~*"
+	print "   ===================================="
+	print "   |     COUNTRY   |-|     CAPITALS    |"
+	print "   |               |-|                 |"
+	for i in al:
+		print i.center(20), al[i].center(20)
+	raw_input("Press Enter ")
+	limpiar()
+	menu()
 
+def allordered():
+	limpiar()
+	print "*~~~~These are your capitals sorted alphabetically~~~*\n"
+	print "     =================================="
+	print "     |    COUNTRY   |-|    CAPITALS   |"
+	print "     |              |-|               |"	
+	ordered = OrderedDict(sorted(al.items(), key=lambda x: x[1:]))
+	for key, value in ordered.items():
+		print key.center(20) + value.center(20)
+	raw_input("press enter to continue")
 
+def allmail():
+	limpiar()
+	select = raw_input("Enter your E-mail: ")
+	select1 = getpass.getpass("Enter your PassWord: ")
+	limpiar()
+	select2 = raw_input("TO: ")
+	select3 = raw_input("Subject: ")
+	limpiar()
+	username = select 
+	password = select1
+	toaddrs  = select2
+	body = "Countries-------\tCapitals\n"
+	for msg in al:
+		body = body + str(msg).center(15) + str(al[msg]).center(15) + "\n"
+	msg = MIMEMultipart()
+	msg['From'] = username
+	msg['To'] = toaddrs
+	msg['Subject'] = select3
+	msg.attach(MIMEText(body, 'plain'))
+	try:
+		server = smtplib.SMTP('smtp.gmail.com:587')
+		server.starttls()
+		server.login(select,select1)
+		text = msg.as_string()
+		server.sendmail(select, toaddrs, text)
+		server.quit()
+		print "Your message was sent"
+		menu()
+	except (smtplib.SMTPAuthenticationError):
+		print "Your email or password are incorrect"
+		raw_input("Press Enter to continue....")
+		allmail()
 def limpiar():
     os.system("reset")
 
 
 def salir():
-    sys.exit()
+	print "Thanks for prefering us"
+	sys.exit()
     
 
 def menu():
     limpiar()
-    print "\t\t\t****Countries and Capitals****\n"
-    print "*~~~What do you want to do?~~~*"
-    print "1. Insert your Country"
-    print "2. Countrie List"
-    print "3. Capital List"
-    print "4. view All"
-    print "5. Exit/Quit\n"
-    menu = raw_input("Insert your option: ")
-
-    if menu == "1":
-        Insert_Country()
-    elif menu == "2":
-        Country_List()
-    elif menu == "3":
-        Capital_List()
-    
-    elif menu == "5":
-        salir()
-    elif menu=="4":
-        view_all()
-    else:
-        print "You have not entered anything"
+    print "\t\t======================================================="
+    print "\t\t||\t(--------------------------------------|       ||"
+    print "\t\t||\t(  ****Countries and Capitals****      |       ||"
+    print "\t\t||\t(--------------------------------------|       ||"
+    print "\t\t||\t    ~~~What do you want to do?~~~*             ||"
+    print "\t\t||\t=========================================      ||"
+    print "\t\t||\t|1. Insert your Country                  |     ||"
+    print "\t\t||\t|2. Country List                         |     ||"
+    print "\t\t||\t|3. Capital List                         |     ||"
+    print "\t\t||\t|4. view All                             |     ||"
+    print "\t\t||\t|5. See all ordered                      |     ||"
+    print "\t\t||\t|6. All by mail                          |     ||"
+    print "\t\t||\t|7. Exit                                 |     ||"
+    print "\t\t||\t==========================================     ||"
+    print "\t\t========================================================"
+    retorno = True
+    while retorno == True:
+	    menu = raw_input("-Insert your option: ")
+	    menu = menu.lower()	
+	    if menu == "1" or menu == "insert":
+	        Insert_Country()
+	        retorno = False
+	    elif menu == "2" or menu == "country" :
+	        Country_List()
+	        retorno = False
+	    elif menu == "3" or menu == "capital":
+	        Capital_List()
+	        retorno = False
+	    elif menu == "4" or menu == "view all":
+	        view_all()
+	        retorno = False
+	    elif menu == "5" or menu == "see all":
+	        allordered()
+	        retorno = False
+	    elif menu == "6" or menu == "all by mail":
+	    	allmail()
+	    	retorno = False
+	    elif menu == "7"or menu == "exit":
+	    	salir()
+	    	retorno = False
+	    elif menu == "":
+	    	print "Please, Enter a valid option."
+	    	retorno = True
+	    else:
+	        print "'You have not entered anything'"
 menu()
