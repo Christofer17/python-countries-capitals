91ba36f93fbfddf0cfc92321f8e38e95ca6044ee
edson1998 <muozperdomo1998.emo@gmail.com>
2015-10-07 11:54:00 -0600

    finished tests

--- application.py
+++ application.py
@@ -1,86 +1,92 @@
+"""This code is for list of countries and capitals"""
 import os
 import sys
 
-al = {}
-Country = []
-Capital = []
+ALL = {}
+COUNTRY = []
+CAPITAL = []
 
 
 def question():
-    var3=raw_input("Do you want to enter another country? y/n: " )
-    var3=var3.lower()
+    """ask the user if you want to enter another country"""
+    var3 = raw_input("Do you want to enter another COUNTRY? y/n: ")
+    var3 = var3.lower()
     if var3 == "y":
-        Insert_Country()
-    elif var3== "n":
+        insert_country()
+    elif var3 == "n":
         menu()
     else:
-        print "This is not valid"
+        print "This is not vALLid"
         limpiar()
         question()
 
-def Insert_Country():
+def insert_country():
+    """Asks the user to enter a country"""
     limpiar()
-    var1 =raw_input("Enter the Country: ")
-    Country.append(var1)
-    var2 =raw_input("Enter the Capital: ")
-    Capital.append(var2)
-    
-    al[var1]=var2
-    question()    
+    var1 = raw_input("Enter the COUNTRY: ")
+    COUNTRY.append(var1)
+    var2 = raw_input("Enter the CAPITAL: ")
+    CAPITAL.append(var2)
+    ALL[var1] = var2
+    question()
     menu()
 
 
-def Country_List():
+def country_list():
+    """Displays the list of countries"""
     limpiar()
-    for i in Country:
-        print i 
+    for i in COUNTRY:
+        print i
     raw_input("Press enter")
     limpiar()
     menu()
 
-def Capital_List():
-    for i in Capital:
-        print i 
+def capital_list():
+    """Display the list of capitals"""
+    for i in CAPITAL:
+        print i
     raw_input("Press enter")
     limpiar()
     menu()
 
 def view_all():
-    for i in al:
-        print i, al[i]
+    """Shows all"""
+    for i in ALL:
+        print i, ALL[i]
     raw_input("presione enter ")
 
-
 def limpiar():
+    """clear"""
     os.system("reset")
 
-
 def salir():
+    """exit the program"""
     sys.exit()
-    
 
 def menu():
+    """Print menu"""
     limpiar()
-    print "\t\t\t****Countries and Capitals****\n"
+    print "\t\t\t****Countries and CAPITALs****\n"
     print "*~~~What do you want to do?~~~*"
-    print "1. Insert your Country"
+    print "1. Insert your COUNTRY"
     print "2. Countrie List"
-    print "3. Capital List"
-    print "4. view All"
+    print "3. CAPITAL List"
+    print "4. view ALLl"
     print "5. Exit/Quit\n"
-    menu = raw_input("Insert your option: ")
+    menu1 = raw_input("Insert your option: ")
 
-    if menu == "1":
-        Insert_Country()
-    elif menu == "2":
-        Country_List()
-    elif menu == "3":
-        Capital_List()
-    
-    elif menu == "5":
+    if menu1 == "1":
+        insert_country()
+    elif menu1 == "2":
+        country_list()
+    elif menu1 == "3":
+        capital_list()
+
+    elif menu1 == "5":
         salir()
-    elif menu=="4":
+    elif menu1 == "4":
         view_all()
     else:
         print "You have not entered anything"
 menu()
+
